1a5192f7ccf8e9b95a7810104f59edeb37b20ac4
Marlon Urriago <maup1993@hotmail.com>
2018-04-28 08:43:05 -0500

    cuarto commit

---
+++ db/schema.rb
@@ -0,0 +1,39 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2018_04_28_133706) do
+
+  create_table "municipios", id: false, force: :cascade do |t|
+    t.integer "codigo"
+    t.string "nombre"
+    t.boolean "estado", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "municipios_regiones", force: :cascade do |t|
+    t.integer "municipio_id"
+    t.integer "region_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["municipio_id"], name: "index_municipios_regiones_on_municipio_id"
+    t.index ["region_id"], name: "index_municipios_regiones_on_region_id"
+  end
+
+  create_table "regiones", id: false, force: :cascade do |t|
+    t.integer "codigo"
+    t.string "nombre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
